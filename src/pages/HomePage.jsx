cb683d99caf7045b20f8ffdde7f8cce81c1a362e
Adrián Maza <adri_mv1@hotmail.com>
2024-07-04 10:07:27 +0200

    advanced course created

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import HeaderComponent from "../components/HeaderComponent";
+
+function HomePage() {
+  return (
+    <>
+      <HeaderComponent></HeaderComponent>
+      <h2>Home Page</h2>
+    </>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/TasksPage.jsx
@@ -0,0 +1,42 @@
+import "./TasksPage.css";
+import { useContext, useEffect } from "react";
+import HeaderComponent from "../components/HeaderComponent";
+import { TaskContext } from "../context/task.context";
+import TaskCard from "../components/TaskCard";
+import CreateTask from "../components/CreateTask";
+
+function TasksPage() {
+  const { tasks, hasLoaded, hasError, getTasks } = useContext(TaskContext);
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const taskCards = tasks.map((task) => (
+    <li key={task.id}>
+      <TaskCard task={task}></TaskCard>
+    </li>
+  ));
+
+  return (
+    <>
+      <HeaderComponent></HeaderComponent>
+      <section id="tasks-page">
+        <h2 className="title">Tasks</h2>
+        <ul className="task-list">
+          <li>
+            <CreateTask></CreateTask>
+          </li>
+          {hasError ? (
+            <h2>No se han podido obtener las tareas</h2>
+          ) : !hasLoaded ? (
+            <h2>Cargando...</h2>
+          ) : (
+            taskCards
+          )}
+        </ul>
+      </section>
+    </>
+  );
+}
+export default TasksPage;
