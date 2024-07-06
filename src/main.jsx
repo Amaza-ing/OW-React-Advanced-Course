21e01aaf4944d7abeee0e0c025a0bfc4d86fdfb5
Adrián Maza <adri_mv1@hotmail.com>
2024-07-06 12:44:34 +0200

    redux-toolkit set up

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
+import { store } from "./redux/store";
 import { Provider } from "react-redux";
-import taskReducer from "./reducers/taskReducer.js";
-
-const store = createStore(taskReducer);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
---
+++ src/redux/taskSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initState = {
+  tasks: [
+    {
+      id: "1",
+      title: "Comprar la cena",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Cocinar",
+      completed: false,
+    },
+    {
+      id: "3",
+      title: "Cenar",
+      completed: false,
+    },
+    {
+      id: "4",
+      title: "Lavar los platos",
+      completed: false,
+    },
+  ],
+  hasLoaded: true,
+  hasError: false,
+};
+
+export const taskSlice = createSlice({
+  name: "task",
+  initialState: initState,
+  reducers: {
+    createTask: (state, action) => {
+      state.tasks.unshift(action.payload);
+    },
+    updateTask: (state, action) => {
+      const taskToUpdate = state.tasks.find(
+        (task) => task.id === action.payload.id
+      );
+      taskToUpdate = action.payload;
+    },
+  },
+});
+
+export const { createTask, updateTask } = taskSlice.actions;
+export default taskSlice.reducer;
